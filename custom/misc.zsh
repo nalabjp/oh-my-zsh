f635aec8312bcc95ae584b1b25565442d3f910f7
nalabjp <nalabjp@gmail.com>
2014-02-16 03:37:14 +0900

    Add EDITOR and TERM env

--- custom/misc.zsh
+++ custom/misc.zsh
@@ -3,3 +3,9 @@ export LIBRARY_PATH=`brew --prefix`/lib:$LIBRARY_PATH
 
 # Homebrew Cask
 export HOMEBREW_CASK_OPTS="--appdir=/Applications --caskroom=/usr/local/Caskroom"
+
+# EDITOR
+export EDITOR='vim'
+
+# TERM
+export TERM=screen-256color
