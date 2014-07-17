604a3dde6194670e1e1d4995c8949dfb02aed604
nalabjp <nalabjp@gmail.com>
2014-07-17 20:38:58 +0900

    Added alias for bundler

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -8,3 +8,13 @@ alias gs='gitsh'
 
 # tmux
 alias tsk='tmux send-keys'
+
+# bundler
+alias b="bundle"
+alias bi="bundle install"
+alias be="bundle exec"
+alias bl="bundle list"
+alias bp="bundle package"
+alias bo="bundle open"
+alias bu="bundle update"
+alias bs="bundle show"
