a3498b885d6af56291a24f82a846fa3db9879c42
nalabjp <nalabjp@gmail.com>
2014-12-02 17:36:08 +0900

    Fix typo

--- custom/misc.zsh
+++ custom/misc.zsh
@@ -12,4 +12,4 @@ export PATH=$HOME/.nodebrew/current/bin:$PATH
 
 # java6
 export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
-exoprt PATH=$JAVA_HOME:$PATH
+export PATH=$JAVA_HOME:$PATH
