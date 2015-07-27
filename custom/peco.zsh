280ccf3fbedb9b96e4f0d93800a969b69fc61ca3
Hajime Kimura <kimura.hajime@moneyforward.co.jp>
2015-07-27 12:34:11 +0900

    Fix bugs

--- custom/peco.zsh
+++ custom/peco.zsh
@@ -33,45 +33,17 @@ zle -N peco-path
 bindkey '^g' peco-path # Ctrl+g で起動
 
 
-function peco-with-vim () {
-  if [[ -n $1 ]]; then
-    BUFFER="vim $1"
-    CURSOR=$#BUFFER
-  fi
-}
-
 function agvim () {
-  peco-with-vim $(ag $@ | peco --query "$LBUFFER" | awk -F : '{print "-c " $2 " " $1}')
+  vim $(ag $@ | peco --query "$LBUFFER" | awk -F : '{print "-c " $2 " " $1}')
 }
 zle -N agvim
 
 function aghvim () {
-  peco-with-vim $(agh $@ | peco --query "$LBUFFER" | awk -F : '{print "-c " $2 " " $1}')
+  vim $(agh $@ | peco --query "$LBUFFER" | awk -F : '{print "-c " $2 " " $1}')
 }
 zle -N aghvim
 
 function ggrvim () {
-  peco-with-vim $(git grep -n $@ | peco --query "$LBUFFER" | awk -F : '{print "-c " $2 " " $1}')
+  vim $(git grep -n $@ | peco --query "$LBUFFER" | awk -F : '{print "-c " $2 " " $1}')
 }
 zle -N ggrvim
-
-
-function peco-rails-command() {
-  local cmd=$(rails $1 -h | sed -ne '/^Please/,$p' | sed '/^ *$/d' | sed -ne '/^ /p' | sed 's/^[ ]*//g' | peco --prompt="[rails $1]")
-  if [[ -n $cmd ]]; then
-    BUFFER="rails $1 $cmd"
-    CURSOR=$#BUFFER
-  fi
-}
-
-function peco-rails-generate() {
-  peco-rails-command generate
-}
-zle -N peco-rails-generate
-alias rrg='peco-rails-generate'
-
-function peco-rails-destroy() {
-  peco-rails-command destroy
-}
-zle -N peco-rails-destroy
-alias rrd='peco-rails-destroy'
